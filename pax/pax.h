c20b7c160116cf46136e6620ff0308082cb0ca85
Randy J. Spaulding <randys006@yahoo.com>
2024-03-26 15:31:24 -0600

    fix email, update GPL

--- pax/pax.h
+++ pax/pax.h
@@ -1,6 +1,6 @@
 /****************************************************************************************************************
  * @file     pax.h
- * @author   Randy J. Spaulding <randys006@gmail.com>
+ * @author   Randy J. Spaulding <randys006@yahoo.com>
  * @version  0.1
  ***********************************************************************************************************/
 /****************************************************************************************************************
@@ -9,7 +9,7 @@
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 of
+ * published by the Free Software Foundation; either version 3 of
  * the License, or (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful, but
